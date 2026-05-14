e702ef5bff0ab60249860b9caec38078ed43c9af
Thufail Al Wafi <hi.fell123@gmail.com>
2026-05-14 20:13:03 +0700

    Add files via upload

---
+++ auth_util.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timedelta
+from typing import Optional
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import HTTPException, status, Depends
+# CONFIGURATION
+SECRET_KEY = "c3c1731c90cb7411abd700cd8b9ea65cddf6a203105d6bda9f18377c6bb0e1de"
+ALGORITHM = "HS256"
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or
+    timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    exc = HTTPException(status_code=401, detail="Invalid credentials")
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        role: str = payload.get("role")
+        if not username or not role: raise exc
+        return {"username": username, "role": role}
+    except JWTError:
+        raise exc
